f8f70dd656ac59d513af73fd99487a161fc8c687
Mike Jones <mike@westerncapelabs.com>
2016-03-04 13:04:27 +0200

    Drop old middleware setting

--- seed_scheduler/settings.py
+++ seed_scheduler/settings.py
@@ -54,7 +54,6 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
