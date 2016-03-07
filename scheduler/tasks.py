feb1f239fd43a4856b86f7a95a0f4d0a35e2d77c
Mike Jones <mike@westerncapelabs.com>
2016-03-07 20:46:45 +0200

    Remove demo task

--- scheduler/tasks.py
+++ scheduler/tasks.py
@@ -9,24 +9,6 @@ from django.conf import settings
 logger = get_task_logger(__name__)
 
 
-class The_Incr(Task):
-
-    """
-    Task to incr something
-    """
-    name = "seed_scheduler.scheduler.tasks.the_incr"
-
-    def run(self, anum, **kwargs):
-        """
-        Returns an incr'd number
-        """
-        l = self.get_logger(**kwargs)
-        l.info("Incrementing <%s>" % (anum,))
-        return int(anum)+1
-
-the_incr = The_Incr()
-
-
 class DeliverHook(Task):
     def run(self, target, payload, instance=None, hook=None, **kwargs):
         """
