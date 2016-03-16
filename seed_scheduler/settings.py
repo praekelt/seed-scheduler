e7d87996df42ccba61273b54e22cb75f8e00de4d
Mike Jones <mike@westerncapelabs.com>
2016-03-16 11:53:06 +0200

    Clean up broker settings

--- seed_scheduler/settings.py
+++ seed_scheduler/settings.py
@@ -142,7 +142,7 @@ HOOK_AUTH_TOKEN = os.environ.get('HOOK_AUTH_TOKEN', 'REPLACEME')
 CELERY_RESULT_BACKEND = 'djcelery.backends.database:DatabaseBackend'
 CELERYBEAT_SCHEDULER = 'djcelery.schedulers.DatabaseScheduler'
 
-BROKER_URL = os.environ.get('RABBITMQ_URL', 'redis://localhost:6379/0')
+BROKER_URL = os.environ.get('BROKER_URL', 'redis://localhost:6379/0')
 
 CELERY_DEFAULT_QUEUE = 'seed_scheduler'
 CELERY_QUEUES = (
