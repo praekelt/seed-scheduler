693b43bdfb38361c7cde3e808f283af203764edd
Jason Paidoussi <jason@paidoussi.net>
2017-01-05 22:07:35 +0200

    Fixed version lookup

--- scheduler/templatetags/seed.py
+++ scheduler/templatetags/seed.py
@@ -1,10 +1,10 @@
 from django import template
 
-import seed_stage_based_messaging
+import seed_scheduler
 
 register = template.Library()
 
 
 @register.simple_tag
 def current_version():
-    return seed_stage_based_messaging.__version__
+    return seed_scheduler.__version__
