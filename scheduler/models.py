6c12bfb2f7a88c6a813f176308458e3957d915f6
Rudi Giesler <rudi@praekelt.org>
2018-12-19 11:35:42 +0200

    Upgrade dependancies excepy for celery

--- scheduler/models.py
+++ scheduler/models.py
@@ -77,7 +77,7 @@ class Schedule(models.Model):
     )
     endpoint = models.CharField(max_length=500, null=False)
     auth_token = models.CharField(max_length=500, null=True, blank=True)
-    payload = JSONField(null=True, blank=True, default={})
+    payload = JSONField(null=True, blank=True, default=dict)
     next_send_at = models.DateTimeField(null=True, blank=True)
     enabled = models.BooleanField(default=True)
     updated_at = models.DateTimeField(auto_now=True)
